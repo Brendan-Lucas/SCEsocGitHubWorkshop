f0b42d06104d2a56e3dcb4efbcf18a819d082eb2
andrew-vicencio <andrew.v1124@gmail.com>
2017-02-05 16:48:13 -0500

    Start program

---
+++ WorkshopProgram.java
@@ -0,0 +1,13 @@
+
+public class WorkshopProgram{
+	public static String name;
+	public static int count;
+	
+	public static void main(String[] args){
+		name = "Andrew";
+		count = 10;
+		for(int i = 0; i <= 10; i++){
+			System.out.println("Make SCESoc great again");
+		}
+	}
+}
