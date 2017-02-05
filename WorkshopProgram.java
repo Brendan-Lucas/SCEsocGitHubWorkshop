0763052338560025719b61c7f7ae520ff5f4c86e
Parallels <brendanwflucas@gmail.com>
2017-02-05 16:00:48 -0500

    add message

---
+++ WorkshopProgram.java
@@ -0,0 +1,9 @@
+
+public class WorkshopProgram{
+    public String name;
+    public static void main(String args[]){
+        for(int i=0; i<10; i++){
+            System.out.println("SCE Soc is great!!!");
+        }
+    }
+}
