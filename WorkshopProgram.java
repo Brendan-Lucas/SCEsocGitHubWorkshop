a9c1a93f1d81330118335c507570f7ddd3146bd3
andrew-vicencio <andrew.v1124@gmail.com>
2017-02-08 19:30:39 -0500

    addnewprint

---
+++ WorkshopProgram.java
@@ -0,0 +1,15 @@
+
+public class WorkshopProgram{
+	public static String name;
+	public static int count;
+	
+	public static void main(String[] args){
+		name = "Andrew";
+		count = 10;
+		for(int i = 0; i <= 10; i++){
+			System.out.println("Make SCESoc great again");
+            		System.out.println("SCE Soc is great!!!");
+			System.out.println(name + " buys a Nintendo switch");
+		}
+	}
+}
